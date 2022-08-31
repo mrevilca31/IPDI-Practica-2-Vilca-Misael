db79201be4243d9b04348d23b4d289aa08094b13
Misael Vilca <mrevilca31@gmail.com>
2022-08-31 11:23:27 -0300

    Se añaden las funciones solicitadas

---
+++ funciones.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+
+''' BIBLIOTECA DE FUNCIONES '''
+
+
+''' PASAJE ENTRE RGB e YIQ '''
+
+
+def pasar_a_YIQ(img):
+    R, G, B = canalesRGB(img)
+
+    Y = (0.299 * R) + (0.587 * G) + (0.114 * B)
+    I = (0.59590059 * R) + (-0.27455667 * G) + (-0.32134392 * B)
+    Q = (0.21153661 * R) + (-0.52273617 * G) + (0.31119955 * B)
+    return (Y, I, Q)
+
+
+def pasar_a_RGB(y, i, q):
+    Y, I, Q = normalizar_YIQ(y, i, q)
+
+    r = (Y+0.9563*I+0.621*Q)*255
+    g = (Y-0.2721*I-0.6474*Q)*255
+    b = (Y-1.1070*I+1.7046*Q)*255
+
+    r = np.clip(r, 0, 255)
+    g = np.clip(g, 0, 255)
+    b = np.clip(b, 0, 255)
+    dimension = Y.shape
+
+    img = np.zeros((dimension[0], dimension[1], 3), dtype='uint8')
+    img[:, :, 0] = r
+    img[:, :, 1] = g
+    img[:, :, 2] = b
+    return(img)
+
+
+def canalesRGB(img):
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+    return (r, g, b)
+
+
+def normalizar_RGB(img):
+    return img/255
+
+
+def normalizar_YIQ(y, i, q):
+    Y = np.clip(y, 0.0, 1.0)
+    I = np.clip(i, -0.5957, 0.5957)
+    Q = np.clip(q, -0.5226, 0.5226)
+    return (Y, I, Q)
+
+
+''' ARITMETICA DE PIXELES '''
+
+
+def cuasiSumaRGB_Clamp(img1, img2):
+    return np.clip(img1 + img2, 0, 1)
+
+
+def cuasiRestaRGB_Clamp(img1, img2):
+    return np.clip(img1 - img2, 0, 1)
+
+
+def cuasiSumaRGB_Prom(img1, img2):
+    return (img1 + img2)/2
+
+
+def cuasiRestaRGB_Prom(img1, img2):
+    return (img1 - img2)/2 + 0.5
+
+
+def cuasiSumaYIQ_Clamp(YA, IA, QA, YB, IB, QB):
+    YC = np.clip(YA + YB, 0, 1)
+    IC = (YA*IA+YB*IB)/(YA+YB)
+    QC = (YA*QA+YB*QB)/(YA+YB)
+    return (YC, IC, QC)
+
+
+def cuasiRestaYIQ_Clamp(YA, IA, QA, YB, IB, QB):
+    YC = np.maximum(YA - YB, 0)
+    IC = (YA*IA-YB*IB)/(YA+YB)
+    QC = (YA*QA-YB*QB)/(YA+YB)
+    return (YC, IC, QC)
+
+
+def cuasiSumaYIQ_Prom(YA, IA, QA, YB, IB, QB):
+    YC = (YA+YB)/2
+    IC = (YA*IA+YB*IB)/(YA+YB)
+    QC = (YA*QA+YB*QB)/(YA+YB)
+    return (YC, IC, QC)
+
+
+def cuasiRestaYIQ_Prom(YA, IA, QA, YB, IB, QB):
+    YC = (YA-YB)/2+0.5
+    IC = (YA*IA-YB*IB)/(YA+YB)
+    QC = (YA*QA-YB*QB)/(YA+YB)
+    return (YC, IC, QC)
+
+
+def productoRGB(img1, img2):
+    ''' recibe valores normalizados '''
+    return np.multiply(img1, img2)
+
+# def cocienteRGB(img1, img2):
+
+
+def restaAbsoluta(img1, img2):
+    return np.abs(img1-img2)
+
+
+def ifLigther(img1, img2):
+    return np.maximum(img1, img2)
+
+
+def ifDarker(img1, img2):
+    return np.minimum(img1, img2)
--- main.py
+++ main.py
@@ -1,56 +1,33 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QImage
+from PyQt5 import QtGui
 from PyQt5.QtCore import *
 import imageio
 import numpy as np
-import matplotlib.pyplot as plt
+from funciones import *
 
 
-# ESTA ES LA CLASE PRINCIPAL
 class pantalla(QMainWindow):
 
     def __init__(self):
-        # Estas dos lineas son obligatorias
         super().__init__()
         uic.loadUi("interfaz.ui", self)
 
-        # Aqui cargo la primera imagen
-        rgb = imageio.imread('imageio:astronaut.png')[:, 50:550, :]
-        #rgb = img*255
-        rgb = np.minimum(rgb, 255)
-        rgb = np.maximum(rgb, 0)
-        h, w, _ = rgb.shape
-        im = QtGui.QImage(rgb.astype(np.uint8), w, h, 3 *
-                          w, QtGui.QImage.Format_RGB888)
-        pix = QtGui.QPixmap.fromImage(im)
-        self.label.setPixmap(pix)
+        img1 = normalizar_RGB(imageio.imread(
+            'imageio:coffee.png')[:, 50:550, :])
+        img2 = normalizar_RGB(imageio.imread(
+            'imageio:astronaut.png')[56:456, 6:506, :])
 
-        # Aqui cargo la seguna imagen
-        rgb = imageio.imread('imageio:coffee.png')[:, 50:550, :]
-        #rgb = img*255
-        rgb = np.minimum(rgb, 255)
-        rgb = np.maximum(rgb, 0)
-        h, w, _ = rgb.shape
-        im = QtGui.QImage(rgb.astype(np.uint8), w, h, 3 *
-                          w, QtGui.QImage.Format_RGB888)
-        pix = QtGui.QPixmap.fromImage(im)
-        self.label_2.setPixmap(pix)
         self.boton.clicked.connect(self.getItem)
-
-    # Funcion para saber cual es la opcion elegida
 
     def getItem(self):
         opc = self.opciones.currentText()
-        # Hace alguna de las operaciones con pixeles
         img = realizarOperacion(opc)
-        # Muestra el resultado
         mostrarResultado(self, img)
 
 
-def mostrarResultado(self, img):
-    rgb = img*255
+def mostrarResultado(self, rgb):
     rgb = np.minimum(rgb, 255)
     rgb = np.maximum(rgb, 0)
     h, w, _ = rgb.shape
@@ -66,14 +43,45 @@ def cuasiSumaRGB_Clamp(img1, img2):
 
 def realizarOperacion(opc):
     if opc == "Suma Clampeada RGB":
-        return cuasiSumaRGB_Clamp(img1, img2)
+        return cuasiSumaRGB_Clamp(img1, img2)*255
+    elif opc == "Suma Promediada RGB":
+        return cuasiSumaRGB_Prom(img1, img2)*255
+    elif opc == "Resta Clampeada RGB":
+        return cuasiRestaRGB_Clamp(img1, img2)*255
+    elif opc == "Resta Promediada RGB":
+        return cuasiRestaRGB_Prom(img1, img2)*255
+    elif opc == "Suma Clampeada YIQ":
+        YA, IA, QA = pasar_a_YIQ(img1)
+        YB, IB, QB = pasar_a_YIQ(img2)
+        YC, IC, QC = cuasiSumaYIQ_Clamp(YA, IA, QA, YB, IB, QB)
+        return pasar_a_RGB(YC, IC, QC)
+    elif opc == "Resta Clampeada YIQ":
+        YA, IA, QA = pasar_a_YIQ(img1)
+        YB, IB, QB = pasar_a_YIQ(img2)
+        YC, IC, QC = cuasiRestaYIQ_Clamp(YA, IA, QA, YB, IB, QB)
+        return pasar_a_RGB(YC, IC, QC)
+    elif opc == "Suma Promediada YIQ":
+        YA, IA, QA = pasar_a_YIQ(img1)
+        YB, IB, QB = pasar_a_YIQ(img2)
+        YC, IC, QC = cuasiSumaYIQ_Prom(YA, IA, QA, YB, IB, QB)
+        return pasar_a_RGB(YC, IC, QC)
+    elif opc == "Resta Promediada YIQ":
+        YA, IA, QA = pasar_a_YIQ(img1)
+        YB, IB, QB = pasar_a_YIQ(img2)
+        YC, IC, QC = cuasiRestaYIQ_Prom(YA, IA, QA, YB, IB, QB)
+        return pasar_a_RGB(YC, IC, QC)
+    elif opc == "Producto":
+        return productoRGB(img1, img2)*255
+    elif opc == "Cociente":
+        pass
+    elif opc == "Resta en Valor Absoluto":
+        return restaAbsoluta(img1, img2)*255
+    elif opc == "If darker":
+        return ifDarker(img1, img2)*255
+    elif opc == "If ligther":
+        return ifLigther(img1, img2)*255
 
 
-#img1 = imageio.imread('imageio:coffee.png')[:, 50:550, :]/255
-#img2 = imageio.imread('imageio:astronaut.png')[56:456, 6:506, :]/255
-
-
-# esto inicia la app
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     GUI = pantalla()
